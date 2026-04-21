4ca64c4ea8b01faa04f1166b8b612332b2b43c71
Shannon Lowe <126143339+SHS-Peb@users.noreply.github.com>
2026-04-21 20:19:40 +1000

    Setup routing pages api (#2)
    * updated Read Me
    
    * Set up routing, base pages, and API service

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import JoinEventPage from "./pages/JoinEventPage";
+import CreateEventPage from "./pages/CreateEventPage";
+
 function App() {
-  return <h1>Recursive Turtles</h1>;
+  return (
+    <Routes>
+      <Route path="/join/:eventCode" element={<JoinEventPage />} />
+      <Route path="/dashboard/events/new" element={<CreateEventPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/JoinEventPage.jsx
@@ -0,0 +1,5 @@
+function JoinEventPage() {
+  return <h1>Join Event Page</h1>;
+}
+
+export default JoinEventPage;
