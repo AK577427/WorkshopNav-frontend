0f4088953ec2f97c62ce1916eea574918900a36c
monicataccoli <monicataccoli@gmail.com>
2026-05-17 23:32:00 +1000

    Fix broken getEventsPerFacilitator import

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Event from "../components/shared/Event";
 import "./DashboardPage.css";
-import { getEventsPerFacilitator } from "../services/events";
+import { getEvents } from "../services/events";
 
 function DashboardPage() {
   const [events, setEvents] = useState([]);
@@ -13,7 +13,7 @@ function DashboardPage() {
   useEffect(() => {
     async function fetchEvents() {
       try {
-        const data = await getEventsPerFacilitator();
+        const data = await getEvents();
         setEvents(data);
       } catch (error) {
         console.error("Error fetching events:", error);
