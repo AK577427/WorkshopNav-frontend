19291117e4ce16970204639fdfb5dab6b3436a62
Anupama Kittur <anupama.kittur@gmail.com>
2026-05-27 01:10:44 +1000

    imported & added route for Questions and QuestionList

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import FacilitatorEventDetailsPage from "./pages/FacilitatorEventDetailsPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import SignupPage from "./pages/SignupPage";
 import ResetPasswordConfirmPage from "./pages/ResetPasswordConfirmPage";
+import QuestionForm from "./components/questions/QuestionForm";
+import QuestionList from "./components/questions/QuestionList";
 
 function App() {
   return (
@@ -51,6 +53,17 @@ function App() {
       <Route
         path="/event/:eventCode"
         element={<AttendeeEventPage />}
+      />
+      {/* QUESTION SUBMISSION */}
+      <Route
+        path="/events/:eventId/questions"
+        element={<QuestionForm />}
+      />
+
+      {/* Get Questions */}
+      <Route
+        path="/events/:eventId/questions"
+        element={<QuestionList />}
       />
 
       {/* EVENT COMPLETE */}
