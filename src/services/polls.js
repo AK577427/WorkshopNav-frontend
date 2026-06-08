b91b516d77adf6bcf68a3561c7bc25abc28d2684
Anupama Kittur <anupama.kittur@gmail.com>
2026-06-09 03:03:49 +1000

    added apis for polls

--- src/services/polls.js
+++ src/services/polls.js
@@ -16,6 +16,10 @@ export function submitPollResponse(pollId, optionId) {
 export function createPoll(eventId, data) {
   return apiRequest(`/events/${eventId}/polls/`, {
     method: "POST",
+    headers: {
+      "Authorization": `Bearer ${window.localStorage.getItem("access")}`,
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       question: data.question,
       options: data.options,
@@ -25,4 +29,39 @@ export function createPoll(eventId, data) {
 
 export function getPollResponses(pollId) {
   return apiRequest(`/polls/${pollId}/responses/`);
+}
+
+export function launchPoll(pollId) {
+  return apiRequest(`/polls/${pollId}/`, {
+    method: "PUT",
+    headers: {
+      "Authorization": `Bearer ${window.localStorage.getItem("access")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      is_active: true
+    })
+  });
+}
+
+export function deletePoll(pollId) {
+  return apiRequest(`/polls/${pollId}/`, {
+    method: "DELETE",
+    headers: {
+      "Authorization": `Bearer ${window.localStorage.getItem("access")}`,
+    },
+  });
+}
+
+export function updatePollStatus(pollId) {
+  return apiRequest(`/polls/${pollId}/`, {
+    method: "PUT",
+    headers: {
+      "Authorization": `Bearer ${window.localStorage.getItem("access")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      is_active: false
+    })
+  });
 }
